4d86010341e03e3742e487a8e073416368fc196d
fernandobaaena <151637085+fernandobaaena@users.noreply.github.com>
2024-11-08 10:56:35 +0100

    commit de migracion

--- database/migrations/2024_10_28_100501_create_cursos_table.php
+++ database/migrations/2024_10_28_100501_create_cursos_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title', 255);
+            $table->string('description', 255);
+            $table->integer('alumRegistred');
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\dashboardController;
 Route::get('/',[dashboardController::class, 'index']) -> name('dashboard');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
